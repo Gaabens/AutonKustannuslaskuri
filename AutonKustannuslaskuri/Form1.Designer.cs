2a0945698a1e33408c55a55aa48d9f845c171e4f
gabriel.savinen <gabriel.savinen@KELABL03>
2024-08-22 12:50:22 +0300

    Add project files.

---
+++ AutonKustannuslaskuri/Form1.Designer.cs
@@ -0,0 +1,286 @@
+﻿namespace AutonKustannuslaskuri
+{
+    partial class KustannusForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            LainaTB = new TextBox();
+            NesteetTB = new TextBox();
+            VakuutuksetTB = new TextBox();
+            PesutTB = new TextBox();
+            PolttoAineTB = new TextBox();
+            MuutKulutTB = new TextBox();
+            RenkaatTB = new TextBox();
+            HuollotTB = new TextBox();
+            KilometritCB = new ComboBox();
+            VastausLB = new Label();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(294, 9);
+            label1.Name = "label1";
+            label1.Size = new Size(376, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Auton kustannuslaskuri";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label2.Location = new Point(12, 87);
+            label2.Name = "label2";
+            label2.Size = new Size(334, 34);
+            label2.TabIndex = 1;
+            label2.Text = "Lainan lyhennys korkoineen:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label3.Location = new Point(12, 142);
+            label3.Name = "label3";
+            label3.Size = new Size(324, 34);
+            label3.TabIndex = 2;
+            label3.Text = "Lisättävät nesteet yms. :";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label4.Location = new Point(12, 201);
+            label4.Name = "label4";
+            label4.Size = new Size(223, 34);
+            label4.TabIndex = 3;
+            label4.Text = "Vakuutusmaksut:";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label5.Location = new Point(12, 254);
+            label5.Name = "label5";
+            label5.Size = new Size(156, 34);
+            label5.TabIndex = 4;
+            label5.Text = "Muut kulut:";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label6.Location = new Point(12, 321);
+            label6.Name = "label6";
+            label6.Size = new Size(142, 34);
+            label6.TabIndex = 5;
+            label6.Text = "Polttoaine:";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label7.Location = new Point(499, 254);
+            label7.Name = "label7";
+            label7.Size = new Size(220, 34);
+            label7.TabIndex = 9;
+            label7.Text = "Kilometrit / vuosi";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label8.Location = new Point(499, 201);
+            label8.Name = "label8";
+            label8.Size = new Size(122, 34);
+            label8.TabIndex = 8;
+            label8.Text = "Renkaat:";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label9.Location = new Point(499, 142);
+            label9.Name = "label9";
+            label9.Size = new Size(104, 34);
+            label9.TabIndex = 7;
+            label9.Text = "Huollot:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            label10.Location = new Point(499, 87);
+            label10.Name = "label10";
+            label10.Size = new Size(90, 34);
+            label10.TabIndex = 6;
+            label10.Text = "Pesut:";
+            // 
+            // LainaTB
+            // 
+            LainaTB.Location = new Point(352, 76);
+            LainaTB.Name = "LainaTB";
+            LainaTB.Size = new Size(114, 52);
+            LainaTB.TabIndex = 10;
+            // 
+            // NesteetTB
+            // 
+            NesteetTB.Location = new Point(352, 134);
+            NesteetTB.Name = "NesteetTB";
+            NesteetTB.Size = new Size(114, 52);
+            NesteetTB.TabIndex = 11;
+            // 
+            // VakuutuksetTB
+            // 
+            VakuutuksetTB.Location = new Point(352, 192);
+            VakuutuksetTB.Name = "VakuutuksetTB";
+            VakuutuksetTB.Size = new Size(114, 52);
+            VakuutuksetTB.TabIndex = 12;
+            // 
+            // PesutTB
+            // 
+            PesutTB.Location = new Point(735, 76);
+            PesutTB.Name = "PesutTB";
+            PesutTB.Size = new Size(114, 52);
+            PesutTB.TabIndex = 13;
+            // 
+            // PolttoAineTB
+            // 
+            PolttoAineTB.Location = new Point(352, 310);
+            PolttoAineTB.Name = "PolttoAineTB";
+            PolttoAineTB.Size = new Size(114, 52);
+            PolttoAineTB.TabIndex = 14;
+            // 
+            // MuutKulutTB
+            // 
+            MuutKulutTB.Location = new Point(352, 250);
+            MuutKulutTB.Name = "MuutKulutTB";
+            MuutKulutTB.Size = new Size(114, 52);
+            MuutKulutTB.TabIndex = 15;
+            // 
+            // RenkaatTB
+            // 
+            RenkaatTB.Location = new Point(735, 190);
+            RenkaatTB.Name = "RenkaatTB";
+            RenkaatTB.Size = new Size(114, 52);
+            RenkaatTB.TabIndex = 16;
+            // 
+            // HuollotTB
+            // 
+            HuollotTB.Location = new Point(735, 134);
+            HuollotTB.Name = "HuollotTB";
+            HuollotTB.Size = new Size(114, 52);
+            HuollotTB.TabIndex = 17;
+            // 
+            // KilometritCB
+            // 
+            KilometritCB.FormattingEnabled = true;
+            KilometritCB.Items.AddRange(new object[] { "5000", "10000", "15000", "20000", "25000", "30000", "35000", "40000", "45000", "50000", "55000", "60000", "65000", "70000", "75000", "80000", "85000", "90000", "95000", "100000" });
+            KilometritCB.Location = new Point(735, 250);
+            KilometritCB.Name = "KilometritCB";
+            KilometritCB.Size = new Size(182, 53);
+            KilometritCB.TabIndex = 19;
+            KilometritCB.SelectedIndexChanged += KilometritCB_SelectedIndexChanged;
+            // 
+            // VastausLB
+            // 
+            VastausLB.AutoSize = true;
+            VastausLB.Font = new Font("Ink Free", 13.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            VastausLB.Location = new Point(24, 415);
+            VastausLB.Name = "VastausLB";
+            VastausLB.Size = new Size(112, 34);
+            VastausLB.TabIndex = 20;
+            VastausLB.Text = "Vastaus";
+            VastausLB.Visible = false;
+            // 
+            // KustannusForm
+            // 
+            AutoScaleDimensions = new SizeF(20F, 45F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1600, 810);
+            Controls.Add(VastausLB);
+            Controls.Add(KilometritCB);
+            Controls.Add(HuollotTB);
+            Controls.Add(RenkaatTB);
+            Controls.Add(MuutKulutTB);
+            Controls.Add(PolttoAineTB);
+            Controls.Add(PesutTB);
+            Controls.Add(VakuutuksetTB);
+            Controls.Add(NesteetTB);
+            Controls.Add(LainaTB);
+            Controls.Add(label7);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label10);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Font = new Font("Ink Free", 17.9999981F, FontStyle.Regular, GraphicsUnit.Point);
+            Margin = new Padding(6, 5, 6, 5);
+            Name = "KustannusForm";
+            Text = "Kilometrikulutuslaskuri";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox LainaTB;
+        private TextBox NesteetTB;
+        private TextBox VakuutuksetTB;
+        private TextBox PesutTB;
+        private TextBox PolttoAineTB;
+        private TextBox MuutKulutTB;
+        private TextBox RenkaatTB;
+        private TextBox HuollotTB;
+        private ComboBox comboBox1;
+        private ComboBox KilometritCB;
+        private Label VastausLB;
+    }
+}
